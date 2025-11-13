e22d4d294b47155dd9ed18b22a6c0e62216a007e
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-13 16:08:53 +0530

    refactor: remove unused file_picker import from feedback submission page

--- lib/presentation/pages/feedback/feedback_submission_page.dart
+++ lib/presentation/pages/feedback/feedback_submission_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
