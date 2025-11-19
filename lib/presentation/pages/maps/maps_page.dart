76035057d2d052ce8305783821431a275c97ae69
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-19 11:44:36 +0530

    Reorder import statements in MapsPage for consistency

--- lib/presentation/pages/maps/maps_page.dart
+++ lib/presentation/pages/maps/maps_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/constants/color_constants.dart';
 
 import '../../../core/constants/color_constants.dart';
 
