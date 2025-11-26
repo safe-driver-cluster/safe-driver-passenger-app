e71b4c7369433e90c0c0a0ed5922dc73b6035769
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-26 17:51:18 +0530

    Add missing imports for BusListPage and DriverListPage in routes.dart

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:safedriver_passenger_app/presentation/pages/buses/bus_list_page.dart';
+import 'package:safedriver_passenger_app/presentation/pages/drivers/driver_list_page.dart';
 import 'package:safedriver_passenger_app/presentation/pages/onboarding/onboarding_page.dart';
 
 import '../presentation/pages/auth/account_verification_page.dart';
@@ -13,6 +15,7 @@ import '../presentation/pages/bus/bus_details_page.dart';
 import '../presentation/pages/bus/bus_history_page.dart';
 import '../presentation/pages/bus/bus_search_page.dart';
 import '../presentation/pages/bus/live_tracking_page.dart';
+import '../presentation/pages/buses/bus_list_page.dart';
 import '../presentation/pages/dashboard/dashboard_page.dart';
 import '../presentation/pages/driver/driver_history_page.dart';
 import '../presentation/pages/driver/driver_info_page.dart';
