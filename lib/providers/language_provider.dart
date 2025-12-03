8d4e887da5e9ac6e6b27c338e218096298049e22
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-03 18:35:17 +0530

    Remove specific import for Locale from dart:ui to improve clarity

--- lib/providers/language_provider.dart
+++ lib/providers/language_provider.dart
@@ -1,4 +1,4 @@
-import 'dart:ui' show Locale;
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
