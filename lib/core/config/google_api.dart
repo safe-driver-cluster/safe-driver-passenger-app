fff519acab28d1df971948d29614b6c78930c690
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-12-03 17:40:33 +0530

    Fix formatting in GoogleMapsConfig class by adding missing newline

--- lib/core/config/google_api.dart
+++ lib/core/config/google_api.dart
@@ -4,6 +4,7 @@ class GoogleMapsConfig {
   // Reads the API key from .env file
   // Make sure to call dotenv.load() in main.dart before using this
   static String get apiKey => dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
-  
+
   static bool get isConfigured => apiKey.isNotEmpty;
 }
+
