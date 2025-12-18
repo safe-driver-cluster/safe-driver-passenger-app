4e049877b823c91335dab985bc8d255251b6981f
Jayali-omobio <jayali.perera@omobio.net>
2025-12-18 20:29:35 +0530

    refactor: add design constants import in FeedbackAnalyticsDashboard

--- lib/presentation/pages/feedback/feedback_analytics_dashboard.dart
+++ lib/presentation/pages/feedback/feedback_analytics_dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../../core/constants/color_constants.dart';
+import '../../../core/constants/design_constants.dart';
 import '../../../data/models/feedback_model.dart';
 
 /// Comprehensive feedback analytics dashboard
