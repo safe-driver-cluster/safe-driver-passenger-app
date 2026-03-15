5df233148f54190e34404321f0fd5f3433834c8a
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-03-15 08:49:25 +0530

    style: Add ignore_for_file directive to suppress print warnings in SmsGatewayService

--- lib/data/services/sms_gateway_service.dart
+++ lib/data/services/sms_gateway_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
